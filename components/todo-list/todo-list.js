b79284124b85cf70355ee0a3e1d938092df23a5c
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2016-11-30 15:23:15 -0800

    working todo-list component

---
+++ components/todo-list/todo-list.js
@@ -0,0 +1,8 @@
+function todoListController(){
+
+}
+
+angular.module('tdlApp').component('todoList', {
+    templateUrl: './components/todo-list/todo-list.html',
+    controller: todoListController
+});
